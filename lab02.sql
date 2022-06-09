9d08436587943ba2c3b516791bb25c0131475903
Swati A Firangi <83855603+SwatiAF@users.noreply.github.com>
2022-06-09 15:29:42 +0530

    bank enterprise

---
+++ lab02.sql
@@ -0,0 +1,24 @@
+create database swatiBankEnterprise;
+
+use swatiBankEnterprise;
+
+create table branch (branch_name varchar(20), branch_city varchar(10), assets int);
+
+alter table branch 
+add primary key(branch_name);
+
+insert into branch values ('branch1', 'city1', 54956);
+insert into branch values ('branch2', 'city2', 84670);
+insert into branch values ('branch3', 'city3', 52356);
+insert into branch values ('branch4', 'city4', 43056);
+insert into branch values ('branch5', 'city5', 28976);
+
+select * from branch;
+
+create table accounts (accno int, 
+branch_name varchar(20), 
+balance int,
+primary key(accno));
+
+alter table accounts 
+add foreign key (branch_name) references branch(branch_name) on delete cascade;
